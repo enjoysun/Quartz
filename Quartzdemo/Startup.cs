dbd7b53b0c8679df21e4d23065cb838500339ce9
enjoysun <1570819807@qq.com>
2017-02-07 14:39:30 +0800

    框架搭建

---
+++ Quartzdemo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Quartzdemo.Startup))]
+namespace Quartzdemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
