2fe5e64bad7bfb8e0139725b8a86221820a03f14
enjoysun <1570819807@qq.com>
2017-02-10 11:31:03 +0800

    学习Jquery插件写法

---
+++ Quartzdemo/Scripts/LearnJqueryApi/colorset.js
@@ -0,0 +1,11 @@
+﻿; (function ($) {
+    $.fn.colorful = function (option) {
+        var defaultset = { color: "blue", fontsize: 20 };
+        var endsetting = $.extend(defaultset, option);
+        this.css({
+            "background-color": "black",
+            "color": endsetting.color,
+            "fontSize": endsetting.fontsize
+        });
+    }
+})(jQuery);
