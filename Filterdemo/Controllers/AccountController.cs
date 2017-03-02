9ccc4ef3666c535b583cbcd1ee3264781f949cf8
enjoysun <1570819807@qq.com>
2017-03-02 17:24:02 +0800

    添加Filter过滤demo

---
+++ Filterdemo/Controllers/AccountController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace Filterdemo.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(string username, string password, string url)
+        {
+            bool result = FormsAuthentication.Authenticate(username, password);
+            if (result)
+            {
+                return Redirect(url ?? Url.Action("Index", "Admin"));
+            }
+            else
+            {
+                ModelState.AddModelError("错误集合","验证错误");
+                return View();
+            }
+        }
+    }
+    public class Userfilter:ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            base.OnActionExecuting(filterContext);
+        }
+        public override void OnResultExecuted(ResultExecutedContext filterContext)
+        {
+            base.OnResultExecuted(filterContext);
+        }
+    }
+}
