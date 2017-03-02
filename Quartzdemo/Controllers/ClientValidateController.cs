d1be243c2a6c8e66a30d5da32e4012ae9d3ca211
enjoysun <1570819807@qq.com>
2017-03-02 11:02:53 +0800

    添加mvc客户端验证demo

---
+++ Quartzdemo/Controllers/ClientValidateController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Quartzdemo.Controllers
+{
+    public class ClientValidateController : Controller
+    {
+        // GET: ClientValidate
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ mvcbroswervalidate/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(mvcbroswervalidate.Startup))]
+namespace mvcbroswervalidate
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
