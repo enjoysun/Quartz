b951b845a20bc8f45a04d4f1184da8d7cf8ca1db
enjoysun <1570819807@qq.com>
2017-03-10 16:13:44 +0800

    添加demo

---
+++ OwinDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OwinDemo
+{
+    class Program
+    {
+        //owin是server和web之间的解耦
+        /// <summary>
+        /// 启动owin实现操作：1.新建owin stratup类
+        ///                   2.注册使用的中间件
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ OwinDemo/Startup1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(OwinDemo.Startup1))]
+
+namespace OwinDemo
+{
+    public class Startup1
+    {
+        //实现owin.selfhost自代理模式
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+        }
+    }
+}
